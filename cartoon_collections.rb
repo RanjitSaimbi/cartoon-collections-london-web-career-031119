44dde0f50ccb567abc3d6afef4ace81ac96bc5b9
Ranjit Saimbi <ranjit.saimbi@gmail.com>
2019-02-20 20:53:12 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,7 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  new_array = []
+  dwarves.each_with_index do |dwarf, index|
+    new_array << puts "#{index + 1}. #{dwarf}"
 end
 
 def summon_captain_planet# code an argument here
