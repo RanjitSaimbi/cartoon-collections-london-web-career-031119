87e2edf2e32ef337c8508e7a15a19b1d4a7503ba
Ranjit Saimbi <ranjit.saimbi@gmail.com>
2019-02-20 21:25:46 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,6 +19,6 @@ def long_planeteer_calls(planeteer_calls)
 end
 
 def find_the_cheese(array_of_food)
-  # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
 end
