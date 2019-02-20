cbfed7a6667a23d29b3d1fb06fc0d3d492540808
Ranjit Saimbi <ranjit.saimbi@gmail.com>
2019-02-20 21:16:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,5 +21,6 @@ end
 def find_the_cheese(array_of_food)
   cheese_types = ["cheddar", "gouda", "camembert"]
   array_of_food.find do |food|
-    
+    food === "cheddar" || "gouda" || "camembert"
+  end 
 end
